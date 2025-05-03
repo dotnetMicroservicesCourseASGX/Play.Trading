ea281e7ac8cb6c8f6e2e66fd1e32c65f31a4b3b5
Andres Suarez Guzman <andres_s18@outlook.com>
2025-05-03 18:08:49 -0500

    initial commit and initializing the state machine

---
+++ src/Play.Trading.Service/Contracts.cs
@@ -0,0 +1,5 @@
+using System;
+
+namespace Play.Trading.Service.Contracts;
+
+public record PurchaseRequested(Guid UserId, Guid ItemId, int Quantity, Guid CorrelationId);
---
+++ src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -0,0 +1,44 @@
+using System;
+using Automatonymous;
+using Play.Trading.Service.Contracts;
+
+namespace Play.Trading.Service.StateMachines;
+
+public class PurchaseStateMachine : MassTransitStateMachine<PurchaseState>
+{
+    public PurchaseStateMachine()
+    {
+        // we find where is that we are going to keep tracking the state of the purchase
+        InstanceState(state => state.CurrentState);
+        ConfigureEvents();
+        ConfigureInitialState();
+    }
+
+    public State Accepted { get; }
+    public State ItemsGranted { get; }
+    public State Completed { get; }
+    public State Faulted { get; }
+    public Event<PurchaseRequested> PurchaseRequested { get; }
+
+    private void ConfigureEvents()
+    {
+        Event(() => PurchaseRequested);
+    }
+
+    private void ConfigureInitialState()
+    {
+        Initially(
+            When(PurchaseRequested)
+                .Then(context =>
+                {
+                    context.Instance.UserId = context.Data.UserId;
+                    context.Instance.ItemId = context.Data.ItemId;
+                    context.Instance.Quantity = context.Data.Quantity;
+                    context.Instance.Received = DateTimeOffset.UtcNow;
+                    context.Instance.LastUpdated = context.Instance.Received;
+                })
+                .TransitionTo(Accepted));
+    }
+
+
+}
